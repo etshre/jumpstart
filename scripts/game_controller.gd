84ba821b471a6aacb9a02da49753b82c5477fa4d
etshre <estherr.w54@gmail.com>
2025-08-07 16:29:42 +0100

    added coins

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -2,6 +2,6 @@ extends Node
 
 var total_coins: int = 0
 
-func coin_collected(value: int);
+func coin_collected(value: int):
 	total_coins += value
-	EventController.emit_signal("coin collected", total_coins)
+	EventController.emit_signal("coin_collected", total_coins)
